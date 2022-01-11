52670a2914579ebae098131b7d18de4b6d9af864
Jaylon Carter <carterjay2001@outlook.com>
2022-01-11 14:55:55 -0600

    added addition subtraction and multiplication

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,15 +6,43 @@ namespace OperatorExercise
     {
         static void Main(string[] args)
         {
-            int a = 17;
-            int b = 4;
-            int quotient = a / b;
-            int remainder = a % b;
+            int a = 7;
+            int b = 8;
+            int add = a + b;
 
-            if (a == 17 && b == 4)
+            if (a == 7 && b == 8)
             {
-                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+                Console.WriteLine($"{a}+{b} is {add}");
             }
+
+            int c = 23;
+            int d = 7;
+            int sub = c - d;
+
+            if (c == 23 && d == 7)
+            {
+                Console.WriteLine($"{c}-{d} is {sub}");
+            }
+
+            int e = 3;
+            int f = 12;
+            int mult = e * f;
+
+            if (e == 3 && f == 12)
+            {
+                Console.WriteLine($"{e}*{f} is {mult}");
+            }
+
+            int g = 17;
+            int h = 4;
+            int quotient = g / h;
+            int remainder = g % h;
+
+            if (g == 17 && h == 4)
+            {
+                Console.WriteLine($"{g}/{h} is {quotient} remainder {remainder}");
+            }
+
 
             Console.WriteLine("\nWhat is the radius of your circle?");
             var radius = double.Parse(Console.ReadLine());
